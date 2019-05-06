9dc17db131986613a9cab6b9635d3be4481342f2
Guido Casiraghi <guido.casiraghi@gmail.com>
2019-05-06 18:03:42 +0200

    Work in progress API, websockets

--- main.go
+++ main.go
@@ -3,27 +3,205 @@ package main
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
 )
 
-// Block is the main block data type
+// Block is the main block Data type
 type Block struct {
-	index        int64
-	timestamp    int64
-	previousHash string
-	data         string
-	hash         string
+	Index        int64  `json:"index"`
+	Timestamp    int64  `json:"timestamp"`
+	PreviousHash string `json:"previousHas"`
+	Data         string `json:"data"`
+	Hash         string `json:"hash"`
 }
 
-// ComputeHash computes the hash of a block struct
-func ComputeHash(block Block) string {
-	s := fmt.Sprintf("%d%d%s%s%s", block.index, block.timestamp, block.previousHash, block.data, block.hash)
+type MessageType int
+
+const (
+	QUERY_LATEST MessageType = iota
+	QUERY_ALL
+	RESPONSE_BLOCKCHAIN
+)
+
+type Message struct {
+	Type *MessageType `json:"type,omitempty"`
+}
+
+type BlockchainResponseMessage struct {
+	Message
+	Blockchain []Block `json:"blockchain",omitempty`
+}
+
+var genesisBlock = Block{0, 0, "1465154705", "my genesis block!!", "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7"}
+var blockchain = []Block{genesisBlock}
+var broadcast = make(chan []byte)
+
+var upgrader = websocket.Upgrader{
+	// TODO check the following
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+var peers = make(map[*websocket.Conn]bool) // TODO process.env.PEERS ? process.env.PEERS.split(',') : [];
+
+// ComputeHash computes the Hash of a block struct
+func computeHashForBlock(block Block) string {
+	return computeHash(block.Index, block.Timestamp, block.PreviousHash, block.Data)
+}
+
+func computeHash(index int64, timestamp int64, previousHash string, data string) string {
+	s := fmt.Sprintf("%d%d%s%s", index, timestamp, previousHash, data)
 	h := sha256.New()
 	h.Write([]byte(s))
-	hash := hex.EncodeToString(h.Sum(nil))
-	return hash
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+func getLatestBlock() Block {
+	return blockchain[len(blockchain)-1]
+}
+
+func isValidNewBlock(newBlock Block, previousBlock Block) (bool, error) {
+	if previousBlock.Index+1 != newBlock.Index {
+		return false, fmt.Errorf("invalid Index")
+	} else if previousBlock.Hash != newBlock.PreviousHash {
+		return false, fmt.Errorf("invalid PreviousHash")
+	} else if computeHashForBlock(newBlock) != newBlock.Hash {
+		return false, fmt.Errorf("invalid Hash")
+	}
+	return true, nil
+}
+
+func addBlock(block Block) error {
+	if ok, err := isValidNewBlock(block, getLatestBlock()); !ok {
+		return err
+	}
+	blockchain = append(blockchain, block)
+	return nil
+}
+
+func getCurrentTimestamp() int64 {
+	return time.Now().UnixNano()
+}
+
+func generateNextBlock(blockData string) Block {
+	previousBlock := getLatestBlock()
+	nextIndex := previousBlock.Index + 1
+	nextTimestamp := getCurrentTimestamp()
+	nextHash := computeHash(nextIndex, nextTimestamp, previousBlock.Hash, blockData)
+	return Block{nextIndex, nextTimestamp, previousBlock.Hash, blockData, nextHash}
+}
+
+func getBlocksEndpoint(w http.ResponseWriter, req *http.Request) {
+	json.NewEncoder(w).Encode(blockchain)
+}
+
+func broadcastHandler(w http.ResponseWriter, r *http.Request) {
+	responseData, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	go writer(responseData)
+}
+
+func peerHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	// register peer
+	peers[conn] = true
+	conn.WriteMessage(websocket.TextMessage, []byte(queryChainLengthMsg()))
+
+	//
+	defer conn.Close()
+	for {
+		_, msgData, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("read error:", err)
+			break
+		}
+		fmt.Println("recv: ", string(msgData))
+		var msg Message
+		err = json.Unmarshal(msgData, &msg)
+		if err != nil {
+			fmt.Println("Error parsing message: ", err)
+			continue
+		}
+		if msg.Type == nil {
+			fmt.Println("Empty message type")
+			continue
+		}
+		switch *msg.Type {
+		case QUERY_LATEST:
+			fmt.Println("QUERY_LATEST")
+			// TODO handle message
+		case QUERY_ALL:
+			fmt.Println("QUERY_ALL")
+			// TODO handle message
+		case RESPONSE_BLOCKCHAIN:
+			fmt.Println("RESPONSE_BLOCKCHAIN")
+			// TODO handle message
+		default:
+			fmt.Println("Unknown message type: ", *msg.Type)
+		}
+	}
+}
+
+func queryChainLengthMsg() string {
+	return "{\"type\": 0}" // TODO
+}
+
+func writer(message []byte) {
+	broadcast <- message
+}
+
+func sender() {
+	for {
+		message := <-broadcast
+		// send to every client that is currently connected
+		// TODO use a mutex
+		for peer := range peers {
+			err := peer.WriteMessage(websocket.TextMessage, message)
+			if err != nil {
+				log.Printf("Websocket error: %s", err)
+				peer.Close()
+				delete(peers, peer)
+			}
+		}
+	}
+}
+
+func initHttpServer() {
+	router := mux.NewRouter()
+	router.HandleFunc("/blocks", getBlocksEndpoint).Methods("GET")
+	// TODO
+	// See: https://www.thepolyglotdeveloper.com/2016/07/create-a-simple-restful-api-with-golang/
+
+	router.HandleFunc("/peer", peerHandler) // websocker
+	router.HandleFunc("/broadcast", broadcastHandler).Methods("POST")
+
+	fmt.Printf("Listening http on port %d", 3000)
+	log.Fatal(http.ListenAndServe(":3000", router))
+}
+
+func initP2PServer() {
+	fmt.Println("Initing P2P...")
+	go sender()
 }
 
 func main() {
-	fmt.Println("Hello, blockchain")
+	fmt.Println("Starting acute blockchain...")
+	initP2PServer()
+	initHttpServer()
+	// TODO websockets peers
+	// See: https://rogerwelin.github.io/golang/websockets/gorilla/2018/03/13/golang-websockets.html
 }
--- main_test.go
+++ main_test.go
@@ -3,19 +3,88 @@ package main
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
+	"math"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 	"time"
 )
 
-func TestComputeHash(t *testing.T) {
-	block := Block{0, time.Now().UnixNano(), "prev", "data", ""}
-	s := fmt.Sprintf("%d%d%s%s%s", block.index, block.timestamp, block.previousHash, block.data, block.hash)
+func TestComputeHashForBlock(t *testing.T) {
+	block := getTestBlock()
+	s := fmt.Sprintf("%d%d%s%s%s", block.Index, block.Timestamp, block.PreviousHash, block.Data, block.Hash)
 	h := sha256.New()
 	h.Write([]byte(s))
 	expected := hex.EncodeToString(h.Sum(nil))
-	got := ComputeHash(block)
+	got := computeHashForBlock(block)
 	if expected != got {
-		t.Errorf("ComputeHash() = %v, want %v", got, expected)
+		t.Errorf("computeHashForBlock() = %v, want %v", got, expected)
 	}
+}
+
+func TestGenerateNextBlock(t *testing.T) {
+	block := generateNextBlock("some block Data")
+	if block.Index != genesisBlock.Index+1 {
+		t.Errorf("generateNextBlock() Index = %v, want %v", block.Index, genesisBlock.Index+1)
+	}
+	if block.PreviousHash != genesisBlock.Hash {
+		t.Errorf("generateNextBlock() PreviousHash = %v, want %v", block.PreviousHash, genesisBlock.Hash)
+	}
+}
+
+func TestGetLatestBlock(t *testing.T) {
+	block := getLatestBlock()
+	if block != genesisBlock {
+		t.Errorf("getLatestBlock() first block not equal to genesis block")
+	}
+}
+
+func TestGetCurrentTimestamp(t *testing.T) {
+	now := time.Now().UnixNano()
+	tolerance := 100.0
+	ts := getCurrentTimestamp()
+	diff := math.Abs(float64(ts) - float64(now))
+	if diff > tolerance {
+		t.Errorf("getCurrentTimestamp() = %v, want %v +/- %v", ts, now, tolerance)
+	}
+}
+
+func getTestBlock() Block {
+	return Block{0, getCurrentTimestamp(), "prev", "Data", ""}
+}
+
+func TestGetBlocksEndpoint(t *testing.T) {
+	// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
+	// pass 'nil' as the third parameter.
+	req, err := http.NewRequest("GET", "/blocks", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(getBlocksEndpoint)
+
+	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+	// directly and pass in our Request and ResponseRecorder.
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// Check the response body is what we expect.
+	j, _ := json.Marshal(blockchain)
+	expected := strings.TrimRight(string(j), "\n")
+	got := strings.TrimRight(rr.Body.String(), "\n")
+	if got != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			got, expected)
+	}
+
 }
